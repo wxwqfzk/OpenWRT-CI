d250f487b3606d73d16e48569e49fd9e7261ffe6
wxwqfzk <56976341+wxwqfzk@users.noreply.github.com>
2024-08-01 23:54:55 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -36,10 +36,10 @@ if [ -n "$WRT_PACKAGE" ]; then
 	echo "$WRT_PACKAGE" >> ./.config
 fi
 
-#高通平台锁定512M内存
+#高通平台锁定1G内存
 if [[ $WRT_TARGET == *"IPQ"* ]]; then
-	echo "CONFIG_ATH11K_MEM_PROFILE_1G=n" >> ./.config
-	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
+	echo "CONFIG_ATH11K_MEM_PROFILE_1G=y" >> ./.config
+	echo "CONFIG_ATH11K_MEM_PROFILE_512M=n" >> ./.config
 fi
 
 #科学插件设置
